fe3e06209af8a0e77f98be1ae4e9c7def12d2bed
anyars-encarta <anyarsencarta@gmail.com>
2023-08-14 13:01:15 +0000

    Setup webpack

---
+++ dist/bundle85e8e6710d263533f446.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log(123);
+/******/ })()
+;
+//# sourceMappingURL=bundle85e8e6710d263533f446.js.map

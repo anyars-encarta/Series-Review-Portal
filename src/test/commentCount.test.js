afc3f9ce7ae4f83c10e84c65e03900f782ef6309
anyars-encarta <anyarsencarta@gmail.com>
2023-08-16 21:44:04 +0000

    Correct test function

--- src/test/commentCount.test.js
+++ src/test/commentCount.test.js
@@ -25,6 +25,23 @@ describe('updateCommentCount', () => {
     expect(document.getElementById('comment-count').textContent).toBe('Comments(0)');
   });
 
+  it('should display zero if no comment is found for an item', async () => {
+    // Mock the comment count element
+    document.body.innerHTML = '<div id="comment-count"></div>';
+
+    // Mock the fetchComments function to return an empty array
+    fetchComments.mockResolvedValue([]);
+
+    // Call the updateCommentCount function with mock appId and itemId
+    await updateCommentCount('mockAppId', 'mockItemId');
+
+    // Check if fetchComments is called with the correct parameters
+    expect(fetchComments).toHaveBeenCalledWith('mockAppId', 'mockItemId');
+
+    // Check if the comment count element is updated with the correct count
+    expect(document.getElementById('comment-count').textContent).toBe('Comments(0)');
+  });
+
   it('should throw an error if fetchComments fails', async () => {
     // Mock the fetchComments function to throw an error
     fetchComments.mockRejectedValue(new Error('Fetch comments failed'));
